be136ca7cd93eaf9a41e6dc721c82f8855657aab
z76316 <z7631614@gmail.com>
2018-09-25 11:48:43 +0800

    Update EC2_express

--- EC2/EC2_express/app.js
+++ EC2/EC2_express/app.js
@@ -39,24 +39,6 @@ app.use(function(err, req, res, next) {
 });
 
 
-// 路徑開頭若為 "/exe/" , server 端就允許這個 cross domain control 的行為
-app.use("/exe/", function(req, res, next) {
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-    res.set("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-    res.set("Access-Control-Allow-Credentials", "true");
-    next();
-});
-
-// Utility
-const request = require("request");
-
-// Test
-app.get('/exe/test',function(req, res) {
-	let data = 'Testing';
-	res.send({ data: data });
-});
-
 var port = 9000;
 app.listen(port, function() {
 	console.log('Server is running on port', port);
