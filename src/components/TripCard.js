4447f22c0d2a2ea44d5c718cfa0e43a8be53805a
z76316 <z7631614@gmail.com>
2018-10-17 21:18:48 +0800

    Implement redis for saving trip list cache of profile page

--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -14,8 +14,6 @@ import '../css/main_profile.css';
 
 // Server ip
 import Server_ip from '../server_ip';
-// let Server_ip = 'http://localhost:9000';
-// let Server_ip = 'http://52.89.137.222:9000';
 
 class TripCard extends Component {
 
