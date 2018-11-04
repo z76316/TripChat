6d68f00deb07279734364e475ace229bce533af4
z76316 <z7631614@gmail.com>
2018-11-04 15:43:31 +0800

    Updated server's donmain again

--- EC2/app.js
+++ EC2/app.js
@@ -47,7 +47,7 @@ let server = app.listen(port, function() {
 
 // "/exe/": allow cross domain control
 app.use("/exe/", function(req, res, next) {
-    res.set("Access-Control-Allow-Origin", "waitforit.tw");
+    res.set("Access-Control-Allow-Origin", "http://waitforit.tw:9000");
     res.set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
     res.set("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
     res.set("Access-Control-Allow-Credentials", "true");
